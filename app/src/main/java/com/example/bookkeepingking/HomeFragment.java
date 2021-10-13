1b473f477fcc4915bc7e837dc0e120c861d22b39
Ken Ryan <Chen97me@gmail.com>
2021-10-12 19:52:11 -0700

    First attempt

--- app/src/main/java/com/example/bookkeepingking/HomeFragment.java
+++ app/src/main/java/com/example/bookkeepingking/HomeFragment.java
@@ -13,15 +13,13 @@ import androidx.navigation.NavHost;
 import androidx.navigation.fragment.NavHostFragment;
 
 public class HomeFragment extends Fragment {
-    TextView showCountTextView;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-        View fragmentFirstLayout = inflater.inflate(R.layout.fragment_home,container,false);
-        showCountTextView = fragmentFirstLayout.findViewById(R.id.textView_one);
+        View fragmentFirstLayout = inflater.inflate(R.layout.fragment_home, container, false);
         // Inflate the layout for this fragment
         return fragmentFirstLayout;
     }
@@ -29,26 +27,6 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.random_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int currentCount = Integer.parseInt(showCountTextView.getText().toString());
-                NavHostFragment.findNavController(HomeFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
-        view.findViewById(R.id.buttonToast).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "@string/chat", Toast.LENGTH_SHORT).show();
-            }
-        });
-        view.findViewById(R.id.buttonCount).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                countMe(view);
-            }
-        });
         view.findViewById(R.id.buttonCalender).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,11 +34,5 @@ public class HomeFragment extends Fragment {
                         navigate(R.id.action_FirstFragment_to_calendarFragment);
             }
         });
-    }
-    private void countMe(View view) {
-        String countString = showCountTextView.getText().toString();
-        Integer count = Integer.parseInt(countString);
-        count++;
-        showCountTextView.setText(count.toString());
     }
 }
